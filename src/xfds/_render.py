c1af3d616b4fa18a2399d2ff0193bd1e995d0675
Brian Cohan <briancohan@gmail.com>
2022-07-28 17:20:03 -0400

    refactored render function

--- src/xfds/_render.py
+++ src/xfds/_render.py
@@ -1,20 +1,14 @@
 """Render Command."""
 from __future__ import annotations
 
-import json
-import pprint
 from itertools import product
 from pathlib import Path
-from typing import Optional, Union
 
-import markdown
-import toml
 import typer
 import yaml
 from jinja2 import Environment
 
-from . import config, log
-from .core import locate_fds_file
+from . import config, errors, log
 
 app = typer.Typer(
     name="render",
@@ -22,72 +16,107 @@ app = typer.Typer(
 )
 
 
-def _normalize(value: str) -> Union[str, float, int]:
-    """Normalize a value.
-
-    Will return as int or float if possible, otherwise will return as string.
-    """
-    for cast in [int, float, str]:
-        try:
-            return cast(value)
-        except ValueError:
-            pass
-
-    return value
-
-
-def get_meta(file_contents: str) -> dict:
-    """Process metadata from the FDS template file.
-
-    Values will be normalized to numbers if possible.
-    If a singular value is present, the value will be
-    returned rather than a list.
-    """
-    md = markdown.Markdown(extensions=["meta"])
-    md.convert(file_contents)
-    meta = md.Meta
-
-    for key, value in meta.items():
-        if len(value) == 1:
-            meta[key] = _normalize(value[0])
-        else:
-            meta[key] = [_normalize(v) for v in value]
-
-    return meta
-
-
-def locate_config(fds_file: Path, meta: dict) -> Optional[Path]:
+def locate_config(cwd: Path) -> Path:
     """Load the configuration file.
 
     Will look for a confing file in the same directory as the FDS template file.
     The config file should have the same base name as the original FDS file.
     If no config file is found, search the FDS meta for a variable "config".
     """
-    for suffix in [".yaml", ".yml", ".toml", ".json"]:
-        config_file = fds_file.with_suffix(suffix)
+    for file in ["pbd.yaml", "pbd.yml"]:
+        config_file = cwd / file
         if config_file.exists():
             return config_file
 
-    if "config" in meta:
-        config_file = Path(meta["config"])
-        if config_file.is_absolute():
-            return config_file
-        return fds_file.parent / config_file
-
-    return None
+    raise errors.ConfigNotFound(f"Could not find Config file pbd.yml in {cwd}.")
 
 
 def read_config(config_file: Path) -> dict:
     """Read the configuration file."""
-    func = {
-        ".yml": yaml.safe_load,
-        ".yaml": yaml.safe_load,
-        ".toml": toml.load,
-        ".json": json.load,
-    }[config_file.suffix]
-
     with config_file.open() as f:
-        return func(f)
+        return yaml.safe_load(f)
+
+
+def _values_match_for_shared_keys(d1: dict, d2: dict) -> bool:
+    keys = set(d1.keys()).intersection(d2.keys())
+    for key in keys:
+        if d1[key] != d2[key]:
+            return False
+    return True
+
+
+def _parse_model(model_spec: dict) -> list[dict]:
+
+    if "file" not in model_spec:
+        raise errors.InputFileNotDefined(
+            "You must specify the input file for each model definition."
+        )
+    if "name" not in model_spec:
+        raise errors.ModelNameNotDefiend(
+            "You must specify the pattern for naming the output files."
+        )
+
+    variables = model_spec.get("variables", dict())
+    params = model_spec.get("parameters", dict())
+    includes = params.get("include", [])
+    excludes = params.get("exclude", [])
+    params = {k: v for k, v in params.items() if k not in ["include", "exclude"]}
+
+    models = []
+    if not params:
+        models.append({"data": variables})
+    else:
+        keys, values = zip(*params.items())
+        for parameters in product(*values):
+            models.append({"data": dict(zip(keys, parameters))})
+
+        for model in models:
+            for include in includes:
+                if _values_match_for_shared_keys(model["data"], include):
+                    model["data"].update(include)
+
+        models = [
+            model
+            for model in models
+            if not any(
+                _values_match_for_shared_keys(model["data"], exclude)
+                for exclude in excludes
+            )
+        ]
+
+    for model in models:
+        data = variables.copy()
+        data.update(model["data"])
+        model["data"] = data
+
+        model.update(
+            {
+                key: value
+                for key, value in model_spec.items()
+                if key not in ["variables", "parameters"]
+            }
+        )
+
+        model["name"] = compile(model["name"], model["data"])
+
+    return models
+
+
+def parse_models(config_data: dict) -> list[dict]:
+    """Create scenarios as defined in the config file."""
+    if "xfds" not in config_data.keys():
+        raise errors.xFDSNotDefined(
+            "'xfds' is not defined as a top-level key in config file."
+        )
+    if "render" not in config_data["xfds"].keys():
+        raise errors.RenderNotDefined(
+            "'render' is not defined under 'xfds' in config file."
+        )
+
+    models = []
+    for model in config_data["xfds"]["render"]:
+        models.extend(_parse_model(model))
+    return models
 
 
 def compile(file_contents: str, data: dict) -> str:
@@ -108,87 +137,23 @@ def write(output_file: Path, contents: str) -> None:
     output_file.write_text(contents)
 
 
-def generate_models(
-    fds_file: Path,
-    template_text: str,
-    model_name: str,
-    model_data: dict,
-    defaults: dict,
-    meta: dict,
-) -> None:
-    """Generate a model."""
-    log.debug(f"Model: {model_name}")
-    log.debug(f"Model data: {model_data}")
-
-    output_dir = fds_file.parent / "output"
-
-    if "matrix" not in model_data:
-        log.warning(f"No matrix specified for model {model_name}")
-        data = meta.copy()
-        data.update(defaults)
-        log.debug(f"=== Data ===\n{pprint.pformat(data)}")
-        contents = compile(template_text, data)
-        output_file = output_dir / model_name / f"{model_name}.fds"
-        write(output_file, contents)
-        return
-
-    keys, values = zip(*model_data["matrix"].items())
-    for matrix_values in product(*values):
-        data = meta.copy()
-        data.update(defaults)
-        data.update(zip(keys, matrix_values))
-        log.debug(f"=== Data ===\n{pprint.pformat(data)}")
-
-        contents = compile(template_text, data)
-        file_name = compile(model_name, data)
-        output_file = output_dir / file_name / f"{file_name}.fds"
-        write(output_file, contents)
-        log.success(f"Created: {output_file}")
-
-
-def main(fds_file: Path) -> None:
-
-    template_text = fds_file.read_text()
-    meta = get_meta(template_text)
-    log.debug(pprint.pformat(meta))
-
-    config_file = locate_config(fds_file, meta)
-    if config_file is None or not config_file.exists():
-        log.warning("No config file found.")
-    else:
-        log.info(f"Config file: {config_file}", icon="📄")
-    config_data = read_config(config_file) if config_file is not None else {}
-
-    defaults = config_data.get("defaults", {}).copy()
-    log.debug(f"Defaults: {defaults}")
-
-    models = config_data.get("models", {fds_file.name: {}}).copy()
-
-    for model_name, model_data in models.items():
-        generate_models(
-            fds_file=fds_file,
-            template_text=template_text,
-            model_name=model_name,
-            model_data=model_data,
-            defaults=defaults,
-            meta=meta,
-        )
-
-
 @app.callback(invoke_without_command=True)
 def render(
-    fds_file: Path = typer.Argument(
-        ".",
-        callback=locate_fds_file,
-        help=(
-            "The FDS file or directory to reset. "
-            "If a **FDS file** is specified, the FDS outputs will be cleared. "
-            "If a **directory** is specified, xFDS will find the first FDS file in the directory "
-            "and assume that is what it should reset. "
-            "if **nothing** is specified, the current directory is used and the above rules are applied. "
-        ),
-    ),
+    directory: Path = typer.Argument(
+        ".", help="Directory containing pbd.yml configuration file."
+    )
 ) -> None:
     """Render an FDS template into scenarios."""
     log.section("Render Command", icon="🖨️ ")
-    main(fds_file)
+
+    config_file = locate_config(Path(directory))
+    log.debug(f"Config File: {config_file}", icon="🛠️")
+
+    config_data = read_config(config_file)
+    models = parse_models(config_data)
+
+    for model in models:
+        input_file = directory / model["file"]
+        output_file = directory / "output" / model["name"] / f"{model['name']}.fds"
+        output_text = compile(input_file.read_text(), model["data"])
+        write(output_file=output_file, contents=output_text)
---
+++ tests/test_render.py
@@ -0,0 +1,256 @@
+from __future__ import annotations
+
+from pathlib import Path
+from textwrap import dedent
+
+import pytest
+import yaml
+from typer.testing import CliRunner
+
+from xfds import _render as render
+from xfds import errors
+from xfds.cli import app
+
+runner: CliRunner = CliRunner()
+
+
+@pytest.fixture
+def datadir_no_config(datadir: Path) -> Path:
+    """Provides reference to no_config directory."""
+    folder = datadir / "no_config"
+    files = list(folder.iterdir())
+    assert len(files) == 1
+    assert files[0].stem != "pbd"
+    return folder
+
+
+@pytest.fixture
+def datadir_no_fds(datadir: Path) -> Path:
+    """Provides reference to no_config directory."""
+    folder = datadir / "no_fds"
+    files = list(folder.iterdir())
+    assert len(files) == 1
+    assert files[0].stem == "pbd"
+    return folder
+
+
+@pytest.fixture
+def datadir_model(datadir: Path) -> Path:
+    """Provides reference to no_config directory."""
+    folder = datadir / "model"
+    files = list(folder.iterdir())
+    assert len(files) == 2
+    return folder
+
+
+########################################
+# Unit Tests
+########################################
+
+# Happy Paths
+def test_finds_config_file(datadir_no_fds: Path) -> None:
+    config = render.locate_config(datadir_no_fds)
+    assert config.stem == "pbd"
+
+
+def test_config_loaded(datadir_model: Path) -> None:
+    config_file = render.locate_config(datadir_model)
+    config = render.read_config(config_file)
+    assert "xfds" in config.keys()
+
+
+def test_model_contains_variables_only_when_no_matrix() -> None:
+    config_text = """
+    xfds:
+      render:
+        - file: model.fds
+          name: model
+          variables:
+            pbd: tools
+    """
+    config = yaml.safe_load(dedent(config_text))
+    scenarios = render.parse_models(config)
+    assert len(scenarios) == 1
+    assert scenarios[0]["data"] == {"pbd": "tools"}
+
+
+def test_model_contains_matrix_and_default_variables() -> None:
+    config_text = """
+    xfds:
+      render:
+        - file: model.fds
+          name: model_{{pbd}}_{{tools}}
+          variables:
+            model: fds
+            pbd: d
+          parameters:
+            pbd: [a, b, c]
+            tools: [1, 2, 3]
+    """
+    config = yaml.safe_load(dedent(config_text))
+    scenarios = render.parse_models(config)
+    assert len(scenarios) == 9
+    assert scenarios[0]["data"] == {"pbd": "a", "tools": 1, "model": "fds"}
+    assert scenarios[-1]["data"] == {"pbd": "c", "tools": 3, "model": "fds"}
+
+
+def test_adds_parameters_with_includes() -> None:
+    config_text = """
+    xfds:
+      render:
+        - file: model.fds
+          name: model_{{pbd}}_{{tools}}
+          variables:
+            model: fds
+          parameters:
+            pbd: [a, b, c]
+            tools: [1, 2, 3]
+            include:
+              - pbd: a
+                tools: 1
+                bar: foo
+              - pbd: c
+                tools: 3
+                foo: bar
+    """
+    config = yaml.safe_load(dedent(config_text))
+    scenarios = render.parse_models(config)
+    assert len(scenarios) == 9
+    assert scenarios[0]["data"] == {
+        "pbd": "a",
+        "tools": 1,
+        "model": "fds",
+        "bar": "foo",
+    }
+    assert scenarios[-1]["data"] == {
+        "pbd": "c",
+        "tools": 3,
+        "model": "fds",
+        "foo": "bar",
+    }
+    assert len(scenarios[1]["data"]) == 3
+
+
+def test_include_overrides_variable() -> None:
+    config_text = """
+    xfds:
+      render:
+        - file: model.fds
+          name: model_{{pbd}}_{{tools}}
+          variables:
+            tools: fds
+          parameters:
+            pbd: [a, b]
+            include:
+              - pbd: a
+                tools: cfast
+    """
+    config = yaml.safe_load(dedent(config_text))
+    scenarios = render.parse_models(config)
+    assert len(scenarios) == 2
+    assert scenarios[0]["data"] == {"pbd": "a", "tools": "cfast"}
+    assert scenarios[1]["data"] == {"pbd": "b", "tools": "fds"}
+
+
+def test_excludes() -> None:
+    config_text = """
+    xfds:
+      render:
+        - file: model.fds
+          name: model_{{pbd}}_{{tools}}
+          variables:
+            model: fds
+          parameters:
+            pbd: [a, b, c]
+            tools: [1, 2, 3]
+            exclude:
+              - pbd: a
+                tools: 1
+              - pbd: b
+    """
+    config = yaml.safe_load(dedent(config_text))
+    scenarios = render.parse_models(config)
+    assert len(scenarios) == 5
+
+
+def test_correct_filenames() -> None:
+    config_text = """
+    xfds:
+      render:
+        - file: model.fds
+          name: model_{{pbd}}_{{tools}}
+          parameters:
+            pbd: [a, b, c]
+            tools: [1, 2]
+            exclude:
+              - pbd: b
+    """
+    config = yaml.safe_load(dedent(config_text))
+    scenarios = render.parse_models(config)
+    assert len(scenarios) == 4
+
+    names = [
+        "model_a_1",
+        "model_a_2",
+        "model_c_1",
+        "model_c_2",
+    ]
+    assert names == [s["name"] for s in scenarios]
+
+
+# Sad Paths
+def test_raises_error_if_no_config(datadir_no_config: Path) -> None:
+    with pytest.raises(errors.ConfigNotFound):
+        render.locate_config(datadir_no_config)
+
+
+def test_error_if_xfds_not_defined() -> None:
+    config_text = """
+    pbd: tools
+    """
+    config = yaml.safe_load(dedent(config_text))
+    with pytest.raises(errors.xFDSNotDefined):
+        render.parse_models(config)
+
+
+def test_error_if_render_not_defined() -> None:
+    config_text = """
+    xfds:
+      pbd: tools
+    """
+    config = yaml.safe_load(dedent(config_text))
+    with pytest.raises(errors.RenderNotDefined):
+        render.parse_models(config)
+
+
+def test_error_if_name_not_specified() -> None:
+    config_text = """
+    xfds:
+      render:
+        - file: model.fds
+    """
+    config = yaml.safe_load(dedent(config_text))
+    with pytest.raises(errors.ModelNameNotDefiend):
+        render.parse_models(config)
+
+
+def test_error_if_file_not_specified() -> None:
+    config_text = """
+    xfds:
+      render:
+        - name: pbdtools
+    """
+    config = yaml.safe_load(dedent(config_text))
+    with pytest.raises(errors.InputFileNotDefined):
+        render.parse_models(config)
+
+
+########################################
+# Integration Tests
+########################################
+@pytest.mark.integration_test
+def test_render_help() -> None:
+    """Ensure render function is available by calling for --help."""
+    runner.invoke(app)
+    result = runner.invoke(app, ["render", "--help"])
+    assert result.exit_code == 0
