19f4b1c50393ef8c6495b7b4f52e3444584ac46c
Brian Cohan <2641449+briancohan@users.noreply.github.com>
2022-08-10 19:03:02 -0400

    Reset (#48)
    * basic render functionality
    
    * matrix rendering
    
    * updated readme
    
    * docs: Add-contributing-information
    
    Provide directions on setting up for development and an overview of the repository structure.
    
    * refactor: Remove unecessary files
    
    * docs: Add contributing articles to nav
    
    * style(extensions): Add useful vs code extensions for development
    
    VS Code will propmt the user for the extensions listed if they're not already installed.
    
    * FDS file syntax documentation
    
    * xfds render flowchart
    
    * reset functionality
    
    * xfds reset tests

--- src/xfds/_reset.py
+++ src/xfds/_reset.py
@@ -1,45 +1,51 @@
 """Reset Command."""
 from __future__ import annotations
 
+from itertools import chain
 from pathlib import Path
 
 import typer
 
 from . import config, log
-from .core import locate_fds_file
+from .core import locate_config, read_config
 
 app = typer.Typer(
     name="reset",
-    help="Reset a folder by clearing everything except the input files.",
+    help="Reset a folder by clearing everything except specified files.",
 )
-ALWAYS_KEEP = [".fds", ".psm", ".zip", ".pbs"]
 
 
 @app.callback(invoke_without_command=True)
 def reset(
-    fds_file: Path = typer.Argument(
+    directory: Path = typer.Argument(
         ".",
-        callback=locate_fds_file,
-        help=(
-            "The FDS file or directory to reset. "
-            "If a **FDS file** is specified, the FDS outputs will be cleared. "
-            "If a **directory** is specified, xFDS will find the first FDS file in the directory "
-            "and assume that is what it should reset. "
-            "if **nothing** is specified, the current directory is used and the above rules are applied. "
-        ),
-    ),
-    chid: str = typer.Option(
-        None, "--chid", help=("If specified, only files matching CHID will be deleted.")
+        help="Directory containing model files.",
     ),
 ) -> None:
     """Stop an FDS simulation."""
     log.section("Reset Command", icon="♻️ ")
 
-    for file in fds_file.parent.iterdir():
-        if file.suffix in ALWAYS_KEEP:
-            log.success(f"Keeping {file.name}")
-            continue
-        if chid is None or file.name.startswith(chid):
-            log.warning(f"Deleting {file.name}")
-            if not config.DRY:
-                file.unlink()
+    config_file = locate_config(directory.parent)
+    config_data = read_config(config_file)
+
+    files_to_keep = list(
+        chain.from_iterable(
+            [
+                directory.glob(pattern)
+                for pattern in config_data["xfds"]["reset"]["keep"]
+            ]
+        )
+    )
+    files_to_delete = [
+        file
+        for file in directory.iterdir()
+        if not file.is_dir() and file not in files_to_keep
+    ]
+
+    for file in files_to_delete:
+        log.warning(f"Deleting {file.name}")
+        if not config.DRY:
+            file.unlink()
+
+    for file in files_to_keep:
+        log.success(f"Keeping {file.name}")
